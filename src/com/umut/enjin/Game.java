3718031327af30615c6e464dbb4785971d49a2da
Umut Köksaldı <umut.4182@gmail.com>
2017-08-19 18:16:30 +0300

    Scaling and Rotation implemented

--- src/com/umut/enjin/Game.java
+++ src/com/umut/enjin/Game.java
@@ -45,8 +45,11 @@ public class Game {
     public void update() {
         temp += Time.getDelta();
 
-        // transform.setTranslation((float) Math.sin(temp), 0, 0);
-        transform.setRotation(0, 0, (float) Math.sin(temp) * 360);
+        float sinTemp = (float) Math.sin(temp);
+
+        transform.setTranslation(sinTemp, 0, 0);
+        transform.setRotation(0, 0,sinTemp * 180);
+        transform.setScale(sinTemp, sinTemp, sinTemp);
     }
 
     public void render() {
--- src/com/umut/enjin/Transform.java
+++ src/com/umut/enjin/Transform.java
@@ -5,6 +5,19 @@ public class Transform {
 
     private Vector3f translation;
     private Vector3f rotation;
+    private Vector3f scale;
+
+    public Vector3f getScale() {
+        return scale;
+    }
+
+    public void setScale(Vector3f scale) {
+        this.scale = scale;
+    }
+
+    public void setScale(float x, float y, float z) {
+        this.scale = new Vector3f(x, y, z);
+    }
 
     public Vector3f getRotation() {
         return rotation;
@@ -32,8 +45,9 @@ public class Transform {
     public Matrix4f getTransformation() {
         Matrix4f transformationMatrix = new Matrix4f().initTranslation(translation.getX(), translation.getY(), translation.getZ());
         Matrix4f rotationMatrix = new Matrix4f().InitRotation(rotation.getX(), rotation.getY(), rotation.getZ());
+        Matrix4f scaleMatrix = new Matrix4f().initScale(scale.getX(), scale.getY(), scale.getZ());
 
-        return transformationMatrix.mul(rotationMatrix);
+        return transformationMatrix.mul(rotationMatrix.mul(scaleMatrix));
     }
 
     public void setTranslation(Vector3f translation) {
